1faddb1cc6dd00164243b457fbe290a5d98086a3
Sviatoslav Pryimak <68956348+0x399@users.noreply.github.com>
2023-04-23 22:36:48 +0300

    Game creation functionality implemented, some visual additions to read page.

--- src/main/java/com/example/reviewer/controller/GameController.java
+++ src/main/java/com/example/reviewer/controller/GameController.java
@@ -7,10 +7,7 @@ import com.example.reviewer.service.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
@@ -27,6 +24,12 @@ public class GameController {
         return gameService.findGamesWithSorting();
     }
 
+    @ModelAttribute(name = "game")
+    public Game game(){
+        Game game = new Game();
+        return game;
+    }
+
     public GameController(GameService gameService) {
         this.gameService = gameService;
     }
@@ -36,17 +39,19 @@ public class GameController {
         return "games-list";
     }
 
+    @GetMapping("/create_game")
+    public String createNewGame(){
+        return "create-game";
+    }
+
+    @PostMapping("/create_game")
+    public String createNewGame(@ModelAttribute("game") Game game){
+        gameService.createGame(game);
+        return "redirect:/games";
+    }
+
     @GetMapping("/{game_id}")
     public String read(@PathVariable("game_id") Long game_id, Model model){
-        Review review = new Review();
-        review.setGame(gameService.getById(game_id));
-        review.setCreatedAt(LocalDateTime.now());
-        review.setUser(new User());
-        review.setDescription("dolbit normalno");
-        review.setScore((byte) 5);
-        Review review1 = review;
-        gameService.getById(game_id).getReviews().add(review);
-        gameService.getById(game_id).getReviews().add(review1);
         model.addAttribute("game", gameService.getById(game_id));
         return "game-info";
     }

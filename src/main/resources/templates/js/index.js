014f4be91e1acf5590f9c251eee982916770438d
NAZARTARANENKO\taran <taranenkonazar0@gmail.com>
2023-06-01 08:31:46 +0300

    Доробляне фінал

--- src/main/resources/templates/js/index.js
+++ src/main/resources/templates/js/index.js
@@ -1,5 +1,17 @@
 function redirect() {
    
-    window.location.href = "новий_url";
+    window.location.href = "create-user.html";
     
   }
+
+  $(document).ready(function() {
+    $('.gameimg').hover(
+      function() {
+        $(this).css('transform', 'scale(1.2)');
+      },
+      function() {
+        $(this).css('transform', 'scale(1)');
+      }
+    );
+  });
+  
